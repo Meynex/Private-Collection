c0637bc14510e87f4e97931af4c7a5a6dc0bda41
Meynex <30909071+Meynex@users.noreply.github.com>
2017-08-10 21:40:38 +0200

    Add files via upload

---
+++ test.sh
@@ -0,0 +1,25 @@
+#!/usr/bin/expect -f
+set address A0:E9:D8:0F:A2:1A
+set temp 1
+spawn bluetoothctl -a
+expect -re "#"
+send "\npower off\r"
+expect {
+		"*succeeded*" { send "power on\r" }
+		"*fail*" { send "power off\r"; send_user "\n scan off Error\n\r" }
+}
+sleep 3
+expect "*power on*"
+send "remove $address\r"
+expect -re "#" { send "\nscan on\r"}
+while {$temp == 1} {
+sleep 15
+
+expect "$address" { send "connect $address\r" }
+
+expect {
+		"*successful*" { send "\nscan off\r"; send "exit\r"; send_user "\nSuccessful.\r\n"; break }
+		"*connect to $address*" { sleep 10 ; exp_continue }
+		-re "#" {send_user "\nwaiting for connection\r" }
+}
+}
